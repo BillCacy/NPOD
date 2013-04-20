5b04292fa9ded473fc0a0a2f66c1cfbe25b97976
Cathy Decoske <cathy.decoske@oeconnection.com>
2013-04-20 18:48:51 -0400

    change form
    change form

--- windows/PicofDay.Designer.cs
+++ windows/PicofDay.Designer.cs
@@ -28,26 +28,72 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.txtimage = new System.Windows.Forms.TextBox();
+            this.txtImageDescr = new System.Windows.Forms.TextBox();
+            this.txtImageTitle = new System.Windows.Forms.TextBox();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.txtDate = new System.Windows.Forms.TextBox();
+            this.lnkURL = new System.Windows.Forms.LinkLabel();
             this.SuspendLayout();
             // 
-            // txtimage
+            // txtImageDescr
             // 
-            this.txtimage.Enabled = false;
-            this.txtimage.Location = new System.Drawing.Point(12, 12);
-            this.txtimage.Multiline = true;
-            this.txtimage.Name = "txtimage";
-            this.txtimage.Size = new System.Drawing.Size(260, 238);
-            this.txtimage.TabIndex = 0;
+            this.txtImageDescr.Location = new System.Drawing.Point(12, 45);
+            this.txtImageDescr.Multiline = true;
+            this.txtImageDescr.Name = "txtImageDescr";
+            this.txtImageDescr.ReadOnly = true;
+            this.txtImageDescr.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtImageDescr.Size = new System.Drawing.Size(291, 205);
+            this.txtImageDescr.TabIndex = 0;
+            // 
+            // txtImageTitle
+            // 
+            this.txtImageTitle.Location = new System.Drawing.Point(12, 18);
+            this.txtImageTitle.Name = "txtImageTitle";
+            this.txtImageTitle.ReadOnly = true;
+            this.txtImageTitle.Size = new System.Drawing.Size(265, 20);
+            this.txtImageTitle.TabIndex = 1;
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(81, 326);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(136, 23);
+            this.btnClose.TabIndex = 2;
+            this.btnClose.Text = "Close Details";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // txtDate
+            // 
+            this.txtDate.Location = new System.Drawing.Point(12, 291);
+            this.txtDate.Name = "txtDate";
+            this.txtDate.ReadOnly = true;
+            this.txtDate.Size = new System.Drawing.Size(265, 20);
+            this.txtDate.TabIndex = 4;
+            // 
+            // lnkURL
+            // 
+            this.lnkURL.AutoSize = true;
+            this.lnkURL.Location = new System.Drawing.Point(12, 262);
+            this.lnkURL.Name = "lnkURL";
+            this.lnkURL.Size = new System.Drawing.Size(55, 13);
+            this.lnkURL.TabIndex = 5;
+            this.lnkURL.TabStop = true;
+            this.lnkURL.Text = "linkLabel1";
+            this.lnkURL.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.lnkURL_LinkClicked);
             // 
             // PicofDay
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
-            this.Controls.Add(this.txtimage);
+            this.ClientSize = new System.Drawing.Size(327, 361);
+            this.Controls.Add(this.lnkURL);
+            this.Controls.Add(this.txtDate);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.txtImageTitle);
+            this.Controls.Add(this.txtImageDescr);
             this.Name = "PicofDay";
-            this.Text = "PicOfDay";
+            this.Text = "NASA Pic Of Day Details";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -55,7 +101,11 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox txtimage;
+        private System.Windows.Forms.TextBox txtImageDescr;
+        private System.Windows.Forms.TextBox txtImageTitle;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.TextBox txtDate;
+        private System.Windows.Forms.LinkLabel lnkURL;
     }
 }
 
--- windows/PicofDay.cs
+++ windows/PicofDay.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 using System.Runtime.InteropServices;
+using System.Diagnostics;
 
 namespace NasaPicOfDay
 {
@@ -15,6 +16,7 @@ namespace NasaPicOfDay
         private System.Windows.Forms.NotifyIcon notifyIcon1;
         private System.Windows.Forms.ContextMenu contextMenu1;
         private System.Windows.Forms.MenuItem menuItem1;
+        private System.Windows.Forms.MenuItem menuItem2;
         [STAThread]
         static void Main()
         {
@@ -22,7 +24,7 @@ namespace NasaPicOfDay
         }
         protected override void OnLoad(EventArgs e)
         {
-  
+
             this.Visible = false; 
             this.ShowInTaskbar = false;
             base.OnLoad(e);
@@ -40,20 +42,25 @@ namespace NasaPicOfDay
 
             this.components = new System.ComponentModel.Container();
             this.contextMenu1 = new System.Windows.Forms.ContextMenu();
+            this.menuItem2 = new System.Windows.Forms.MenuItem();
             this.menuItem1 = new System.Windows.Forms.MenuItem();
 
             // Initialize contextMenu1
             this.contextMenu1.MenuItems.AddRange(
-            new System.Windows.Forms.MenuItem[] { this.menuItem1 });
+            new System.Windows.Forms.MenuItem[] { this.menuItem2, this.menuItem1  });
 
             // Initialize menuItem1
             this.menuItem1.Index = 0;
             this.menuItem1.Text = "E&xit";
             this.menuItem1.Click += new System.EventHandler(this.menuItem1_Click);
+            // Initialize menuItem2
+            this.menuItem2.Index = 0;
+            this.menuItem2.Text = "&See Details";
+            this.menuItem2.Click += new System.EventHandler(this.menuItem2_Click);
 
             // Set up how the form should be displayed.
-            this.ClientSize = new System.Drawing.Size(292, 266);
-            this.Text = "Notify Icon Example";
+            //this.ClientSize = new System.Drawing.Size(300,400);
+            //this.Text = backgroundImage.ImageTitle;
 
             // Create the NotifyIcon.
             this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
@@ -68,10 +75,15 @@ namespace NasaPicOfDay
 
             // The Text property sets the text that will be displayed,
             // in a tooltip, when the mouse hovers over the systray icon.
-           // notifyIcon1.Text = "Nasa Pic of the day app";
             notifyIcon1.Text = backgroundImage.ImageTitle;
             notifyIcon1.Visible = true;
-            this.txtimage.Text = backgroundImage.ImageDescription;
+            this.txtImageDescr.Text = backgroundImage.ImageDescription;
+            this.txtImageTitle.Text = backgroundImage.ImageTitle;
+            this.lnkURL.Text = "Link to this Image";
+            this.lnkURL.Links.Add(0, this.lnkURL.Text.Length, backgroundImage.ImageUrl);
+                 
+            this.txtDate.Text = backgroundImage.ImageDate.ToLongDateString();
+           
 
             // Handle the DoubleClick event to activate the form.
             notifyIcon1.DoubleClick += new System.EventHandler(this.notifyIcon1_DoubleClick);
@@ -96,6 +108,7 @@ namespace NasaPicOfDay
                 this.WindowState = FormWindowState.Normal;
 
             // Activate the form.
+            this.Visible = true;
             this.Activate();
 
         }
@@ -108,6 +121,25 @@ namespace NasaPicOfDay
     private void menuItem1_Click(object Sender, EventArgs e) {
         // Close the form, which closes the application.
         this.Close();
+    }
+    private void menuItem2_Click(object Sender, EventArgs e)
+    {
+        // Activate the form.
+
+        this.Visible = true;
+        //this.Activate();
+    }
+    private void btnClose_Click(object sender, EventArgs e)
+    {
+        //make the form invisible
+        this.Visible = false;
+
+    }
+
+    private void lnkURL_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+    {
+        System.Diagnostics.Process.Start(e.Link.LinkData.ToString());
+
     }
 
     }
