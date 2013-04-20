8c226f6e683973e5a8245b7b3b6010d7ad533f17
Bill Cacy <wcacy1@hotmail.com>
2013-04-20 15:49:31 -0400

    Implemented call to change background

--- windows/NasaPicOfDay/NasaPicOfDay.cs
+++ windows/NasaPicOfDay/NasaPicOfDay.cs
@@ -1,33 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-using System.Data;
-using System.Runtime.InteropServices;
 
 namespace NasaPicOfDay
 {
 
-
 	class SetWallpaper
 	{
-		[DllImport("user32.dll", CharSet = CharSet.Auto)]
-		private static extern Int32 SystemParametersInfo(UInt32 uiAction, UInt32 uiParam, String pvParam, UInt32 fWinIni);
-		private static UInt32 SPI_SETDESKWALLPAPER = 20;
-		private static UInt32 SPIF_UPDATEINIFILE = 0x1;
-
-
-
 		static void Main(string[] args)
 		{
 			BackgroundChanger changer = new BackgroundChanger();
 			BackgroundImage backgroundImage = changer.GetTodaysImage();
 			changer.SetDesktopBackground(backgroundImage.DownloadedPath);
-
 		}
-
 	}
-
-
-
-
 }
