db4e69151abb05a97f1752f1c6fa50de07907b09
Bill Cacy <wcacy1@hotmail.com>
2013-04-20 15:05:17 -0400

    Implemented background image change

--- windows/NasaPicOfDay/NasaPicOfDay.cs
+++ windows/NasaPicOfDay/NasaPicOfDay.cs
@@ -8,25 +8,24 @@ namespace NasaPicOfDay
 {
 
 
-    class SetWallpaper
-    { 
-        [DllImport("user32.dll", CharSet = CharSet.Auto)]
-private static extern Int32 SystemParametersInfo(UInt32 uiAction, UInt32 uiParam, String pvParam, UInt32 fWinIni);
-private static UInt32 SPI_SETDESKWALLPAPER = 20;
-private static UInt32 SPIF_UPDATEINIFILE = 0x1;
-private static string imageFileName = "C:\\nasaimages\\NASA_ImageOfDay.bmp";
+	class SetWallpaper
+	{
+		[DllImport("user32.dll", CharSet = CharSet.Auto)]
+		private static extern Int32 SystemParametersInfo(UInt32 uiAction, UInt32 uiParam, String pvParam, UInt32 fWinIni);
+		private static UInt32 SPI_SETDESKWALLPAPER = 20;
+		private static UInt32 SPIF_UPDATEINIFILE = 0x1;
 
 
-        static void Main(string[] args)
-        {
-            setImage(imageFileName);
 
-        }
-      public static void setImage( string filename )
-        {
-        SystemParametersInfo(SPI_SETDESKWALLPAPER, 0, filename, SPIF_UPDATEINIFILE);
-        } 
-    }
+		static void Main(string[] args)
+		{
+			BackgroundChanger changer = new BackgroundChanger();
+			BackgroundImage backgroundImage = changer.GetTodaysImage();
+			changer.SetDesktopBackground(backgroundImage.DownloadedPath);
+
+		}
+
+	}
 
 
 
