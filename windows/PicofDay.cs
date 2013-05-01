47737cf0dad0a76884673a22f8b88fc7cb33d1b7
Bill Cacy <william.cacy@oeconnection.com>
2013-05-01 15:08:37 -0400

    Functionality Update
    Added an Update option to the context menu to allow background image
    updates without having to close and re-open the application. I've found
    recently that the images seem to update mid-day. Also cleaned up form
    variables.

--- windows/PicofDay.cs
+++ windows/PicofDay.cs
@@ -13,10 +13,11 @@ namespace NasaPicOfDay
 {
 	public partial class PicofDay : Form
 	{
-		private System.Windows.Forms.NotifyIcon notifyIcon1;
-		private System.Windows.Forms.ContextMenu contextMenu1;
-		private System.Windows.Forms.MenuItem menuItem1;
-		private System.Windows.Forms.MenuItem menuItem2;
+		private NotifyIcon notifyIcon1;
+		private ContextMenu contextMenu1;
+		private MenuItem exitMenuItem;
+		private MenuItem detailsMenuItem;
+        private MenuItem updateMenuItem;
 		private Timer appTimer;
 		[STAThread]
 		static void Main()
@@ -56,78 +57,89 @@ namespace NasaPicOfDay
 		{
 			try
 			{
-				BackgroundChanger changer = new BackgroundChanger();
-				if (changer == null)
-					throw new Exception("Error retrieving current image information.");
+                this.components = new System.ComponentModel.Container();
+                this.contextMenu1 = new ContextMenu();
+                this.detailsMenuItem = new MenuItem();
+                this.exitMenuItem = new MenuItem();
+                this.updateMenuItem = new MenuItem();
+                // Create the NotifyIcon.
+                this.notifyIcon1 = new NotifyIcon(this.components);
 
-				BackgroundImage backgroundImage = changer.GetTodaysImage();
-				changer.SetDesktopBackground(backgroundImage.DownloadedPath);
+                // The Icon property sets the icon that will appear
+                // in the systray for this application.
+                notifyIcon1.Icon = new Icon("trayIcon.ico");
 
-				this.components = new System.ComponentModel.Container();
-				this.contextMenu1 = new System.Windows.Forms.ContextMenu();
-				this.menuItem2 = new System.Windows.Forms.MenuItem();
-				this.menuItem1 = new System.Windows.Forms.MenuItem();
+                // The ContextMenu property sets the menu that will
+                // appear when the systray icon is right clicked.
+                notifyIcon1.ContextMenu = this.contextMenu1;
 
+                // Handle the DoubleClick event to activate the form.
+                notifyIcon1.DoubleClick += new System.EventHandler(this.notifyIcon1_DoubleClick);
+
+                UpdateContent();
 
 				// Initialize contextMenu1
 				this.contextMenu1.MenuItems.AddRange(
-				new System.Windows.Forms.MenuItem[] { this.menuItem2, this.menuItem1 });
+				new MenuItem[] { this.detailsMenuItem, this.updateMenuItem, this.exitMenuItem });
 
-				// Initialize menuItem1
-				this.menuItem1.Index = 0;
-				this.menuItem1.Text = "E&xit";
-				this.menuItem1.Click += new System.EventHandler(this.menuItem1_Click);
-				// Initialize menuItem2
-				this.menuItem2.Index = 0;
-				this.menuItem2.Text = "&See Details";
-				this.menuItem2.Click += new System.EventHandler(this.menuItem2_Click);
+				// Initialize exitMenuItem
+				this.exitMenuItem.Index = 0;
+				this.exitMenuItem.Text = "E&xit";
+				this.exitMenuItem.Click += new System.EventHandler(this.exitMenuItem_Click);
+				// Initialize detailsMenuItem
+				this.detailsMenuItem.Index = 0;
+				this.detailsMenuItem.Text = "&See Details";
+				this.detailsMenuItem.Click += new System.EventHandler(this.detailsMenuItem_Click);
+                // Initialize updateMenuItem
+                this.updateMenuItem.Index = 0;
+                this.updateMenuItem.Text = "Update";
+                this.updateMenuItem.Click += new EventHandler(updateMenuItem_Click);
 
 				// Set up how the form should be displayed.
-				//this.ClientSize = new System.Drawing.Size(300,400);
-				//this.Text = backgroundImage.ImageTitle;
+                this.TopMost = true;
 
-				// Create the NotifyIcon.
-				this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
-
-				// The Icon property sets the icon that will appear
-				// in the systray for this application.
-				notifyIcon1.Icon = new Icon("trayIcon.ico");
-
-				// The ContextMenu property sets the menu that will
-				// appear when the systray icon is right clicked.
-				notifyIcon1.ContextMenu = this.contextMenu1;
-
-				// The Text property sets the text that will be displayed,
-				// in a tooltip, when the mouse hovers over the systray icon.
-
-				/* 4/28/2012 - Bill Cacy: Checking Image title for length > 64 characters.
-				 * Titles that are longer than 64 characters cause an error and the tray icon
-				 * does not load*/
-				if (backgroundImage.ImageTitle.Length >= 63)
-					notifyIcon1.Text = backgroundImage.ImageTitle.Substring(0, 63);
-				else
-					notifyIcon1.Text = backgroundImage.ImageTitle;
-
-				notifyIcon1.Visible = true;
-				this.txtImageDescr.Text = backgroundImage.ImageDescription;
-				this.txtImageTitle.Text = backgroundImage.ImageTitle;
-				this.lnkURL.Text = "Link to this Image";
-				this.lnkURL.Links.Add(0, this.lnkURL.Text.Length, backgroundImage.ImageUrl);
-
-				this.txtDate.Text = backgroundImage.ImageDate.ToLongDateString();
-
-				// Handle the DoubleClick event to activate the form.
-				notifyIcon1.DoubleClick += new System.EventHandler(this.notifyIcon1_DoubleClick);
+				
 			}
 			catch (Exception ex)
 			{
 				ExceptionManager.WriteException(ex);
 			}
 		}
+
+        private void UpdateContent()
+        {
+            BackgroundChanger changer = new BackgroundChanger();
+            if (changer == null)
+                throw new Exception("Error retrieving current image information.");
+
+            BackgroundImage backgroundImage = changer.GetTodaysImage();
+            changer.SetDesktopBackground(backgroundImage.DownloadedPath);
+
+            // The Text property sets the text that will be displayed,
+            // in a tooltip, when the mouse hovers over the systray icon.
+
+            /* 4/28/2012 - Bill Cacy: Checking Image title for length > 64 characters.
+             * Titles that are longer than 64 characters cause an error and the tray icon
+             * does not load*/
+            if (backgroundImage.ImageTitle.Length >= 63)
+                this.notifyIcon1.Text = backgroundImage.ImageTitle.Substring(0, 63);
+            else
+                this.notifyIcon1.Text = backgroundImage.ImageTitle;
+
+            this.notifyIcon1.Visible = true;
+            this.txtImageDescr.Text = backgroundImage.ImageDescription;
+            this.txtImageTitle.Text = backgroundImage.ImageTitle;
+            this.lnkURL.Text = "Link to this Image";
+            this.lnkURL.Links.Clear();
+            this.lnkURL.Links.Add(0, this.lnkURL.Text.Length, backgroundImage.ImageUrl);
+
+            this.txtDate.Text = backgroundImage.ImageDate.ToLongDateString();
+        }
+
 		void appTimer_Tick(object sender, EventArgs e)
 		{
 			//Reload the application content
-			LoadApplicationContent();
+            UpdateContent();
 		}
 		private void notifyIcon1_DoubleClick(object Sender, EventArgs e)
 		{
@@ -148,18 +160,22 @@ namespace NasaPicOfDay
 				ExceptionManager.WriteException(ex);
 			}
 		}
-		private void menuItem1_Click(object Sender, EventArgs e)
+		private void exitMenuItem_Click(object Sender, EventArgs e)
 		{
 			// Close the form, which closes the application.
 			appTimer.Stop();
 			appTimer = null;
 			this.Close();
 		}
-		private void menuItem2_Click(object Sender, EventArgs e)
+		private void detailsMenuItem_Click(object Sender, EventArgs e)
 		{
 			// Activate the form.
 			this.Visible = true;
 		}
+        private void updateMenuItem_Click(object sender, EventArgs e)
+        {
+            UpdateContent();
+        }
 		private void btnClose_Click(object sender, EventArgs e)
 		{
 			//make the form invisible
