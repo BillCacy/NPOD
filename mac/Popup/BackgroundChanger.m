92fa4d9e1fd37014ec09f03676fd05eb9f56d013
Brandon Ruffridge <bruffridge@gmail.com>
2013-08-01 14:19:50 -0400

    Updated BackgroundChanger for new JSON webservice.

--- mac/Popup/BackgroundChanger.m
+++ mac/Popup/BackgroundChanger.m
@@ -6,7 +6,7 @@
 
 -(NSArray *)setWallpaper
 {
-    
+    // 
     // get first <ig> under root. This is the latest image of the day.
     // get the first <ap>. This contains a link to the xml file for the image.
     // append http://www.nasa.gov to the value.
@@ -16,71 +16,50 @@
     
     //download http://www.nasa.gov/multimedia/imagegallery/iotdxml.xml
     NSError *err = nil;
+    
+    
     NSArray *titleDesc = nil;
-    NSURL *myURL = [NSURL URLWithString:@"http://www.nasa.gov/multimedia/imagegallery/iotdxml.xml"];
-    NSXMLDocument *iotdxml = [[NSXMLDocument alloc] initWithContentsOfURL:myURL options:0 error:&err];
-    // Root is rss/channel
-    // get first <ig> under root. This is the latest image of the day.
-    // get the first <ap>. This contains a link to the xml file for the image.
-    NSArray *nodes = [iotdxml nodesForXPath:@"./rss[1]/channel[1]/ig[1]/ap[1]"
-                                      error:&err];
-    if ([nodes count] > 0 ) {
-        // do something with element
-        NSXMLElement *latestImgXML = [nodes objectAtIndex:0];
-        NSString *latestImgXMLHref = [latestImgXML stringValue];
-        
-        // append http://www.nasa.gov to the value.
-        NSString *baseURL = @"http://www.nasa.gov";
-        latestImgXMLHref = [baseURL stringByAppendingString:latestImgXMLHref];
-        latestImgXMLHref = [latestImgXMLHref stringByAppendingString:@".xml"];
-        NSLog(@"%@",latestImgXMLHref);
-        
-        
-        // download the file.
-        myURL = [NSURL URLWithString:latestImgXMLHref];
-        iotdxml = [[NSXMLDocument alloc] initWithContentsOfURL:myURL options:0 error:&err];
-        
-        NSString *iotdTitleString = @"";
-        NSString *iotdDescriptionString = @"";
-        
-        // get the title
-        nodes = [iotdxml nodesForXPath:@"./rss[1]/channel[1]/title[1]"
-                                 error:&err];
-        if ([nodes count] > 0 ) {
-            // do something with element
-            NSXMLElement *iotdTitleElement = [nodes objectAtIndex:0];
-            iotdTitleString = [iotdTitleElement stringValue];
+    NSURL *myURL = [NSURL URLWithString:@"http://www.nasa.gov/ws/image_gallery.jsonp?format_output=1&display_id=page_1&limit=50&offset=0&Routes=1446"];
+    
+    NSData* data = [NSData dataWithContentsOfURL:myURL options:0 error:&err];
+    
+    if(data) {
+        id object = [NSJSONSerialization
+                     JSONObjectWithData:data
+                     options:0
+                     error:&err];
+        if (err) {
+            NSLog(@"%@",[err localizedDescription]);
         }
-        
-        // get the description
-        nodes = [iotdxml nodesForXPath:@"./rss[1]/channel[1]/description[1]"
-                                 error:&err];
-        if ([nodes count] > 0 ) {
-            // do something with element
-            NSXMLElement *iotdDescriptionElement = [nodes objectAtIndex:0];
-            iotdDescriptionString = [iotdDescriptionElement stringValue];
-        }
-        
-        titleDesc = [NSArray arrayWithObjects:iotdTitleString, iotdDescriptionString, nil ];
-        
-        //[iotdTitle setStringValue:iotdTitleString];
-        //[iotdDescription setStringValue:iotdDescriptionString];
-        
-        // get the <href> in <image> - <size> with <type>Full_Size</type>
-        nodes = [iotdxml nodesForXPath:@"./rss[1]/channel[1]/image[1]/size[type=\"Full_Size\"][1]/href"
-                                 error:&err];
-        if ([nodes count] > 0 ) {
-            // do something with element
-            NSXMLElement *iotdHrefElement = [nodes objectAtIndex:0];
-            NSString *iotdHrefString = [iotdHrefElement stringValue];
+        if([object isKindOfClass:[NSDictionary class]])
+        {
+            NSDictionary *results = object;
+            NSArray    *nodes = [results objectForKey: @"nodes"];
+            NSDictionary   *firstNode = [nodes objectAtIndex: 0];
+            NSDictionary *latestImageObj = [firstNode objectForKey: @"node"];
+            
+            // get the title and description
+            NSString     *iotdTitleString = [latestImageObj objectForKey: @"title"];
+            NSString     *iotdDescriptionString = [latestImageObj objectForKey: @"image_caption"];
+                        
+            titleDesc = [NSArray arrayWithObjects:iotdTitleString, iotdDescriptionString, nil ];
+            
+            //[iotdTitle setStringValue:iotdTitleString];
+            //[iotdDescription setStringValue:iotdDescriptionString];
+            
+            
+            NSString     *imageURL = [latestImageObj objectForKey: @"master_image"];
             
             // append http://www.nasa.gov to the value.
-            iotdHrefString = [baseURL stringByAppendingString:iotdHrefString];
-            NSLog(@"%@",iotdHrefString);
+            NSString *baseURL = @"http://www.nasa.gov";
+            imageURL = [baseURL stringByAppendingString:imageURL];
+            //NSLog(@"%@",imageURL);
+            
+            // http://www.nasa.gov/sites/default/files/pia17397.jpg
             
             // Download the file and set as wallpaper.
             // Create the request.
-            NSURLRequest *theRequest=[NSURLRequest requestWithURL:[NSURL URLWithString:iotdHrefString]
+            NSURLRequest *theRequest=[NSURLRequest requestWithURL:[NSURL URLWithString:imageURL]
                                                       cachePolicy:NSURLRequestUseProtocolCachePolicy
                                                   timeoutInterval:60.0];
             // create the connection with the request
@@ -93,14 +72,13 @@
             } else {
                 // Inform the user that the connection failed.
             }
-        }
-        if (err != nil) {
-            NSLog(@"%@",[err localizedDescription]);
+
         }
     }
-    if (err != nil) {
+    if (err) {
         NSLog(@"%@",[err localizedDescription]);
     }
+    
     return titleDesc;
 }
 
