b0d625e9ce04729d06112ea63aa8177c00f4d143
Brandon Ruffridge <bruffridge@gmail.com>
2014-02-07 12:31:36 -0500

    fixed login items duplicating

--- mac/Popup/ApplicationDelegate.m
+++ mac/Popup/ApplicationDelegate.m
@@ -43,6 +43,7 @@ void *kContextActivePanel = &kContextActivePanel;
 - (void)applicationDidFinishLaunching:(NSNotification *)notification
 {
     // add app to login items.
+    [self deleteAppFromLoginItem];
     [self addAppAsLoginItem];
     
     // Install icon into the menu bar
@@ -245,6 +246,37 @@ void *kContextActivePanel = &kContextActivePanel;
 	}
     
 	CFRelease(loginItems);
+}
+
+-(void) deleteAppFromLoginItem{
+	NSString * appPath = [[NSBundle mainBundle] bundlePath];
+    
+	// This will retrieve the path for the application
+	// For example, /Applications/test.app
+	CFURLRef url = (__bridge CFURLRef)[NSURL fileURLWithPath:appPath];
+    
+	// Create a reference to the shared file list.
+	LSSharedFileListRef loginItems = LSSharedFileListCreate(NULL,
+                                                            kLSSharedFileListSessionLoginItems, NULL);
+    
+	if (loginItems) {
+		UInt32 seedValue;
+		//Retrieve the list of Login Items and cast them to
+		// a NSArray so that it will be easier to iterate.
+		NSArray  *loginItemsArray = (__bridge NSArray *)LSSharedFileListCopySnapshot(loginItems, &seedValue);
+		int i = 0;
+		for(i ; i< [loginItemsArray count]; i++){
+			LSSharedFileListItemRef itemRef = (__bridge LSSharedFileListItemRef)[loginItemsArray
+                                                                        objectAtIndex:i];
+			//Resolve the item with URL
+			if (LSSharedFileListItemResolve(itemRef, 0, (CFURLRef*) &url, NULL) == noErr) {
+				NSString * urlPath = [(__bridge NSURL*)url path];
+				if ([urlPath compare:appPath] == NSOrderedSame){
+					LSSharedFileListItemRemove(loginItems,itemRef);
+				}
+			}
+		}
+	}
 }
 
 - (void)connection:(NSURLConnection *)connection didReceiveResponse:(NSURLResponse *)response
